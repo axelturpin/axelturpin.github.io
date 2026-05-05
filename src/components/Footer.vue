849bf21928705da79589db374f7668ee21eb9078
axelturpin <axelturpin@hotmail.com>
2026-05-05 16:02:54 +0200

    footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -12,9 +12,9 @@ export default{
         <div>Inspiré de <a href="https://imgflip.com/memegenerator" class="lien">Imgflip Meme Generator</a>
             et <a href="https://neal.fun/absurd-trolley-problems/" class="lien">Neal.fun ("absurd-trolley-problems" de Neal Agarwal)</a>
         </div>
-        <div>Dilemmes Inspirés avec scénarios adaptés et reformulés.: 1 trolley problem de Philippa Foot (1967)
+        <div>Dilemmes Inspirés avec scénarios adaptés et reformulés: 1 trolley problem de Philippa Foot (1967)
             2 Étude de Spranca, Minsk & Baron (1991)
-            3 Dilemme des pilules
+            3 Dilemmes des pilules, Descriptions de Mr phi sur youtube
         </div>
         <a href="https://www.flaticon.com/fr/icones-gratuites/etoile" title="étoile icônes" class="lien">Étoile icônes créées par Pixel perfect - Flaticon</a>
     </footer>
