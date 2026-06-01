1013f32227cba86c9d3580fffbc8d900cf1b1fdf
axelturpin <axelturpin@hotmail.com>
2026-06-01 18:39:22 +0200

    étoiles jaunes

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -17,7 +17,7 @@ export default{
         <a target="_blank" href="https://icons8.com" class="lien">Icons8</a>
     </div>
     <div>
-        <a href="https://www.flaticon.com/fr/icones-gratuites/ui" title="ui icônes">Ui icônes créées par iconmas - Flaticon</a>
+        <a href="https://www.flaticon.com/fr/icones-gratuites/ui" title="ui icônes" class="lien">Ui icônes créées par iconmas - Flaticon</a>
     </div>
 
     <!-- Inspirations de contenu -->
